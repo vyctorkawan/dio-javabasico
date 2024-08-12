7caab55a6bb761ce24db14e2a9f5e5043ccb68e8
Vyctor <vyctor.kkdm@gmail.com>
2024-08-12 16:09:31 -0300

    módulo sobre tipos de variáveis

---
+++ java-var-tipos/src/tipo_variaveis.java
@@ -0,0 +1,14 @@
+public class tipo_variaveis {
+    public static void main(String[] args) throws Exception {
+       
+        String texto = "Armazena texto"; 
+        int inteiro = 1; //armazena números inteiros
+        long longo = 1000000000000000000L; //armazena números maiores, com mais bytes (finalizar com L)
+        short curto = 10000; //armazena números menores para utilizar menos bytes
+
+        double ponto = 3.15; //armazena numeros fracionados com até 8 bytes
+        float flutuante = 3.15F; //armazena fracionados até 4 bytes (sempre finalizar com F)
+
+
+    }
+}
